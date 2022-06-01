60a2f0906128d4f1e2b03e0700a58f59ef8f9a81
limhyejin <hejin8307@naver.com>
2022-06-01 21:08:02 +0900

    connect firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import './app.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import styles from './app.module.css';
+import Home from './components/home/home';
+import Login from './components/login/login';
 
-function App() {
-  return <h1>hello</h1>;
+function App({authService}) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={
+            <div className={styles.login}>
+              <Login authService={authService} />
+            </div>
+          }
+        />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/service/auth_service.js
@@ -0,0 +1,32 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from 'firebase/auth';
+
+class AuthService {
+  constructor() {
+    this.firebaseAuth = getAuth();
+    this.googleAuthProvider = new GoogleAuthProvider();
+    this.githubAuthProvider = new GithubAuthProvider();
+  }
+
+  login(providerName) {
+    const authProvider = this.getProvider(providerName);
+    return signInWithPopup(this.firebaseAuth, authProvider);
+  }
+
+  getProvider(providerName) {
+    switch (providerName) {
+      case 'Google':
+        return this.googleAuthProvider;
+      case 'Github':
+        return this.githubAuthProvider;
+      default:
+        throw new Error(`not supported provider: ${providerName}`);
+    }
+  }
+}
+
+export default AuthService;
